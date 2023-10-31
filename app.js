1287fcb35d67d763f2a29ebeb38de0d038fcee33
maximomarigo <maximomarigo@gmail.com>
2023-10-31 17:26:52 -0300

    probando

--- app.js
+++ app.js
@@ -2,8 +2,28 @@ require("dotenv").config();
 const { createBot, createProvider, createFlow } = require("@bot-whatsapp/bot");
 const QRPortalWeb = require("@bot-whatsapp/portal");
 const BaileysProvider = require("@bot-whatsapp/provider/baileys");
-const MockAdapter = require("@bot-whatsapp/database/mock");
+const MySQLAdapter = require('@bot-whatsapp/database/mysql');
 
+
+
+
+
+/**
+ * Base de Datos
+ */
+const MYSQL_DB_HOST = '127.0.0.1';
+const MYSQL_DB_USER = 'root';
+const MYSQL_DB_PASSWORD = 'contraseña';
+const MYSQL_DB_NAME = 'chatbot_gescom';
+const MYSQL_DB_PORT = '3308';
+
+const adapterDB = new MySQLAdapter({
+    host: MYSQL_DB_HOST,    
+    user: MYSQL_DB_USER,
+    database: MYSQL_DB_NAME,
+    password: MYSQL_DB_PASSWORD,
+    port: MYSQL_DB_PORT,
+});
 /**
  * ChatGPT
  */
